6b3e19fde19b0de44fe8ef802c85282d46f397ca
tayronjim <tayrom@gmail.com>
2024-06-03 12:36:55 -0600

    initial

---
+++ test1/test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Tayron Jimenez on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
